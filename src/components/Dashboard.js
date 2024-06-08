0f6e664904610c4a5c6e5b8e24d82a26b929189f
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-09 00:30:21 +0530

    added logout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert, Button, Card } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 
-const Dashboard = () => {
-	return <div>Dashboard</div>;
-};
+export default function Dashboard() {
+	const [error, setError] = useState("");
+	const { currentUser, logout } = useAuth();
 
-export default Dashboard;
+	const navigate = useNavigate();
+	async function handleLogout() {
+		setError("");
+
+		try {
+			await logout();
+			navigate("/login");
+		} catch {
+			setError("Failed to log out");
+		}
+	}
+	return (
+		<>
+			<Card>
+				<Card.Body>
+					<h2 className='text-center mb-4'>profile</h2>
+					{error && <Alert variant='danger'>{error}</Alert>}
+					<strong>Email: </strong> {currentUser.email}
+					<Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+						Update
+					</Link>
+				</Card.Body>
+			</Card>
+
+			<div className='w-100 text-center mt-2'>
+				<Button variant='link' onClick={handleLogout}>
+					Log out
+				</Button>
+			</div>
+		</>
+	);
+}
